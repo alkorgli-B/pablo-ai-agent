12366c05fd555dad124199b4212a799f256b03b6
alkorgli-B <alkorgli26@gmail.com>
2026-02-11 12:31:36 +0200

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,13 @@
+module.exports = async function handler(req, res) {
+  return res.status(200).json({
+    message: "API is working!",
+    query: req.query,
+    url: req.url,
+    env_check: {
+      has_twitter_key: !!process.env.TWITTER_API_KEY,
+      has_anthropic_key: !!process.env.ANTHROPIC_API_KEY,
+      has_bot_secret: !!process.env.BOT_SECRET_KEY,
+      bot_secret_value: process.env.BOT_SECRET_KEY,
+    }
+  });
+};
